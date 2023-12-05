61d1ede3a5f290c3788e4e2d71c72ada98956a93
Danylo Reznytskyi <danilrez.it@gmail.com>
2023-12-05 17:00:00 +0200

    second

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const toggleButton = document.querySelector('.toggle-button');
 const asideMenu = document.querySelector('.aside-menu');
 
 toggleButton.addEventListener('click', function () {
-		asideMenu.style.display = (asideMenu.style.display === 'block' || 'flex') ? 'none' : 'block';
-	});
+	asideMenu.style.display = (asideMenu.style.display === 'block' || asideMenu.style.display === 'flex') ? 'none' : 'block';
+});
 
 			 function handleResize() {
 					 const windowHeight = window.innerHeight;
